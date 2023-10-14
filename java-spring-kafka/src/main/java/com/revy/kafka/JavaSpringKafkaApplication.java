aefd78d3e54a2d0c693b362f261d60c25ca0906c
Revy <wantedhooni@gmail.com>
2023-10-15 05:57:19 +0900

    INIT: 프로젝트 기본 셋팅

---
+++ java-spring-kafka/src/main/java/com/revy/kafka/JavaSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.revy.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringKafkaApplication.class, args);
+    }
+
+}
---
+++ java-spring-kafka/src/test/java/com/revy/kafka/JavaSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revy.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
